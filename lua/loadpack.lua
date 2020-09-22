954ec4d4b86888bef8dd11714534b449c5802e53
Raphael <glepnir@gopherhub.org>
2020-09-22 15:19:04 +0800

    test packer

--- lua/loadpack.lua
+++ lua/loadpack.lua
@@ -1,16 +1,22 @@
 local M = {}
+local global = require('global')
 
 function M.load_repos()
-  local plugin_dir = os.getenv("HOME")..'/.cache/vim/plugins'
+  local plugin_dir = os.getenv("HOME")..'/.cache/vim/plugins/'
+  local packer_dir = plugin_dir .. 'packer.nvim'
+  local cmd = "git clone https://github.com/wbthomason/packer.nvim " ..packer_dir
   if vim.fn.has('vim_starting') then
-    vim.o.runtimepath = vim.o.runtimepath .. ','..plugin_dir ..'/packer.nvim'
+    if not global.isdir(packer_dir) then
+      os.execute(cmd)
+    end
+    vim.o.runtimepath = vim.o.runtimepath .. ','..packer_dir
+    vim.o.packpath = vim.o.packpath .. ','..plugin_dir..'packer'
   end
   local packer = require('packer')
   local use = packer.use
   packer.init({package_root = plugin_dir})
   packer.reset()
   use {'glepnir/dashboard-nvim'}
-  packer.install()
 end
 
 return M
