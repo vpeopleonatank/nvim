ff7c0a6968eea1c93d1f8247ae18709f67af28eb
vpoat <vpoat@pop-os.localdomain>
2021-02-25 15:04:43 +0700

    comment lolcat

--- lua/modules/ui/config.lua
+++ lua/modules/ui/config.lua
@@ -17,9 +17,9 @@ end
 
 function config.dashboard()
   local home = os.getenv('HOME')
-  vim.g.dashboard_preview_command = 'cat'
-  vim.g.dashboard_preview_pipeline = 'lolcat'
-  vim.g.dashboard_preview_file = home .. '/.config/nvim/static/pokemon.txt'
+  -- vim.g.dashboard_preview_command = 'cat'
+  -- vim.g.dashboard_preview_pipeline = 'lolcat'
+  -- vim.g.dashboard_preview_file = home .. '/.config/nvim/static/pokemon.txt'
   vim.g.dashboard_preview_file_height = 14
   vim.g.dashboard_preview_file_width = 80
   vim.g.dashboard_default_executive = 'telescope'
