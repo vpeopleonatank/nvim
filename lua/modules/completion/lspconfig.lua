87578ef3d9792c0aae240852a71391f5d3f9751b
vpoat <vpoat@pop-os.localdomain>
2021-05-08 19:47:30 +0700

    add temporarily efm server

--- lua/modules/completion/lspconfig.lua
+++ lua/modules/completion/lspconfig.lua
@@ -147,7 +147,7 @@ lspconfig.pyright.setup {
 	 settings = {
       python = {
         analysis = {
-          typeCheckingMode = "basic",
+          typeCheckingMode = "off",
           autoSearchPaths = true,
           useLibraryCodeForTypes = true
         }
@@ -169,6 +169,87 @@ lspconfig.jsonls.setup {
         }
     }
 }
+
+local python_arguments = {}
+
+local flake8 = {
+    LintCommand = "flake8 --ignore=E501 --stdin-display-name ${INPUT} -",
+    lintStdin = true,
+    lintFormats = {"%f:%l:%c: %m"}
+}
+
+local isort = {formatCommand = "isort --quiet -", formatStdin = true}
+
+local yapf = {formatCommand = "yapf --quiet", formatStdin = true}
+local black = {formatCommand = "black --quiet -", formatStdin = true}
+
+table.insert(python_arguments, flake8)
+table.insert(python_arguments, isort)
+table.insert(python_arguments, black)
+
+local luaFormat = {
+    formatCommand = "lua-format -i --no-keep-simple-function-one-line --column-limit=120",
+    formatStdin = true
+}
+
+local lua_fmt = {
+    formatCommand = "luafmt --indent-count 2 --line-width 120 --stdin",
+    formatStdin = true
+}
+
+local shfmt = {formatCommand = 'shfmt -ci -s -bn', formatStdin = true}
+
+local shellcheck = {
+    LintCommand = 'shellcheck -f gcc -x',
+    lintFormats = {'%f:%l:%c: %trror: %m', '%f:%l:%c: %tarning: %m', '%f:%l:%c: %tote: %m'}
+}
+
+local prettier = {formatCommand = "prettier --stdin-filepath ${INPUT}", formatStdin = true}
+
+local eslint = {
+    lintCommand = "./node_modules/.bin/eslint -f unix --stdin --stdin-filename ${INPUT}",
+    lintIgnoreExitCode = true,
+    lintStdin = true,
+    lintFormats = {"%f:%l:%c: %m"},
+    formatCommand = "./node_modules/.bin/eslint --fix-to-stdout --stdin --stdin-filename=${INPUT}",
+    formatStdin = true
+}
+
+local tsserver_args = {}
+table.insert(tsserver_args, prettier)
+table.insert(tsserver_args, eslint)
+
+local markdownPandocFormat = {formatCommand = 'pandoc -f markdown -t gfm -sp --tab-stop=2', formatStdin = true}
+
+-- Not yet get efm server work
+-- lspconfig.efm.setup {
+--     -- init_options = {initializationOptions},
+--     cmd = {global.data_path .. "/lspinstall/efm/efm-langserver"},
+--     init_options = {documentFormatting = true, codeAction = false},
+--     filetypes = {"lua", "python", "javascriptreact", "javascript", "typescript",
+--                 "typescriptreact","sh", "html", "css", "json", "yaml", "markdown", "vue"},
+--     -- filetypes = {"lua", "python", "sh", "html", "css", "json", "yaml", "markdown"},
+--     settings = {
+--         rootMarkers = {".git/"},
+--         languages = {
+--             python = python_arguments,
+--             lua = luaFormat,
+--             sh = {shfmt},
+--             javascript = tsserver_args,
+--             javascriptreact = tsserver_args,
+-- 			typescript = tsserver_args,
+-- 			typescriptreact = tsserver_args,
+--             html = {prettier},
+--             css = {prettier},
+--             json = {prettier},
+--             yaml = {prettier},
+--             markdown = {markdownPandocFormat}
+--             -- javascriptreact = {prettier, eslint},
+--             -- javascript = {prettier, eslint},
+--             -- markdown = {markdownPandocFormat, markdownlint},
+--         }
+--     }
+-- }
 
 local servers = {
 'bashls', 'html', 'cssls',
