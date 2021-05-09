2b6d0a98dddd11f8adf0ae482299522fc37eb1ec
vpoat <vpoat@pop-os.localdomain>
2021-05-09 20:36:14 +0700

    up

--- lua/modules/lang/plugins.lua
+++ lua/modules/lang/plugins.lua
@@ -14,6 +14,13 @@ lang['nvim-treesitter/nvim-treesitter-textobjects'] = {
   after = 'nvim-treesitter'
 }
 
+lang['romgrk/nvim-treesitter-context'] = {
+  after = 'nvim-treesitter'
+}
+
+-- lang['code-biscuits/nvim-biscuits'] = {
+--   after = 'nvim-treesitter'
+-- }
 -- lang['JoosepAlviste/nvim-ts-context-commentstring'] = {
 --   require = 'nvim-treesitter'
 -- }
