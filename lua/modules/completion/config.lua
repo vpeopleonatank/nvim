901f4c21d15617c7ea4f835cf6844beec7a8f2b7
vpoat <vpoat@pop-os.localdomain>
2021-05-13 10:52:52 +0700

    back to fzy

--- lua/modules/completion/config.lua
+++ lua/modules/completion/config.lua
@@ -105,22 +105,22 @@ function config.telescope()
       qflist_previewer = require'telescope.previewers'.vim_buffer_qflist.new,
     },
     extensions = {
-        -- fzy_native = {
-        --     override_generic_sorter = false,
-        --     override_file_sorter = true,
-        -- }
-      fzf = {
-        override_generic_sorter = false, -- override the generic sorter
-        override_file_sorter = true,     -- override the file sorter
-        case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
-                                       -- the default case_mode is "smart_case"
-      }
+        fzy_native = {
+            override_generic_sorter = false,
+            override_file_sorter = true,
+        }
+      -- fzf = {
+      --   override_generic_sorter = false, -- override the generic sorter
+      --   override_file_sorter = true,     -- override the file sorter
+      --   case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+      --                                  -- the default case_mode is "smart_case"
+      -- }
     }
   }
 
   require'telescope'.load_extension('project')
-  -- require('telescope').load_extension('fzy_native')
-  require('telescope').load_extension('fzf')
+  require('telescope').load_extension('fzy_native')
+  -- require('telescope').load_extension('fzf')
   require'telescope'.load_extension('dotfiles')
   require'telescope'.load_extension('gosource')
 end
